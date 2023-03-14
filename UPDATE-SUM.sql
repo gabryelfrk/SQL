8d5d22419c2ab1732e5ec8fce5ad584ec95b3d1f
gabryelfrk <84803703+gabryelfrk@users.noreply.github.com>
2023-03-14 16:51:26 -0300

    Update UPDATE-SUM.sql

--- UPDATE-SUM.sql
+++ UPDATE-SUM.sql
@@ -2,9 +2,9 @@ UPDATE s
 SET s.bascalicm = (
 	SELECT SUM(r.xbc_icms) FROM rcdpe r 
 	WHERE r.xnumero = s.nota
-	AND s.esp = r.xesp 
-	AND s.serie = r.xserie 
-	AND s.codfisca = r.xcodfisca 
+	AND r.xesp = s.esp
+	AND r.xserie = s.serie
+	AND r.xcodfisca = s.codfisca
 )
 FROM saida s
 WHERE s.diames >= '01-01-2023' AND s.diames <= '31-01-2023'
